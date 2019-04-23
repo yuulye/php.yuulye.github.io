6ef8f0e7576cb5f0b10324396a7a849be1ccbf97
yuulye <yuulye@gmail.com>
2019-04-23 13:45:09 +0700

    add research print

---
+++ resources/views/print.blade.php
@@ -0,0 +1,5 @@
+@extends('base')
+
+@section('head')
+  <style>@include('print.style')</style>
+@endsection
---
+++ resources/views/research/print.blade.php
@@ -0,0 +1,21 @@
+@extends('print')
+
+@section('head')
+  @parent
+  <style>
+    .borderTest {
+      width: 100%;
+      height: 100%;
+      border: 1px solid black;
+    }
+  </style>
+@endsection
+
+@section('content')
+@for ($i=0; $i < 100; $i++)
+<div class="page">
+  <div class="borderTest"></div>
+</div>
+<br/>
+@endfor
+@endsection
