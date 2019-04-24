5966a20c8a67eb79328f7cc11e0262c57224abcd
yuulye <yuulye@gmail.com>
2019-04-24 11:33:53 +0700

    wudu init layout

--- resources/views/research/wudu.blade.php
+++ resources/views/research/wudu.blade.php
@@ -1,5 +1,20 @@
 @extends('print')
 
+@section('head')
+  @parent
+
+  <style>
+    table tr td {
+      width: 50%;
+      vertical-align: top;
+      padding: 8px;
+    }
+    table tr td.bordered {
+      border-right: 1px solid rgba(0, 0, 0, .7);
+    }
+  </style>
+@endsection
+
 @section('content')
 @for ($i = 0; $i < 10; $i++)
 <div class="page">
--- resources/views/research/wudu/1.blade.php
+++ resources/views/research/wudu/1.blade.php
@@ -1,100 +1,152 @@
-مَنْ تَوَضَّأَ فَأَحْسِنُ الوُضُوْءَ خَرَجَتْ خَطَايَاهُ مِنْ جَسَدِهِ حَتَّى تَخْرُجُ مِنْ تَحْتَ أَظْفَارَهَ
+<table>
+  <tr>
+    <td class="bordered">
+    </td>
+    <td>
+      <h2>
+      Doa Ketika Melihat Air
+      </h2>
+    </td>
+  </tr>
+  <tr>
+    <td class="bordered">
+      @include('research.wudu.1-1')
+    </td>
+    <td>
+      @include('research.wudu.melihat_air')
+    </td>
+  </tr>
+  <tr>
+    <td class="bordered">
+      <hr/>
 Artinya:
 Rasulullah bersabda, "Barangsiapa berwudhu dengan membaguskan wudhu'nya, maka keluarlah dosa-dosanya dari kulitnya sampai  kuku jari-jemarinya".
-
-Doa Ketika Melihat Air
-
-اَلْحَمْدُ ِللهِ الَّذِي جَعَلَ اْلمَاءَ طَهُوْرًا
+    </td>
+    <td>
+      <hr/>
 Al-hamdu lillahilladzi ja'alal-ma'a tahuran
-
+      <hr/>
 Artinya :
 "Segala puji bagi Allah yang telah menjadikan Air ini suci
+    </td>
+  </tr>
+</table>
+<hr/>
 
+<h2>
 Doa Ketika Membasuh Telapak Tangan
-
+</h2>
+<div class="arabic">
 اَللّٰهُمَّ احْفkَظْ يَدَيَّ مِنْ مَعَاصِكَ كُلِّهَا
+</div>
 Allohummahfadz Yadayya Min Ma'asyika Kulliha
-
+<br/>
 Artinya :
 "Ya Allah peliharalah kedua tanganku dari semua perbuatan maksiat pada-Mu"
+<br/>
 
+<h2>
 Doa Saat Berkumur
-
+</h2>
+<div class="arabic">
 اَللّٰهُمَّ أَعِنِّي عَلَى ذِكْرِكَ وَشُكْرِكَ وَحُسْنِ عِبَادَتِكَ
-
+</div>
 Allohumma a'inni 'Ala Dzikrika wa Syukrika wahusni 'Ibadatika
+<br/>
 Artinya:
 "Ya Allah tolonglah aku untuk selalu ingat kepadaMu dan selalu bersyukur serta memperbaiki ibadah kepadaMu"
+<br/>
 
+<h2>
 Doa Ketika Menghirup Air Ke Hidung
-
+</h2>
+<div class="arabic">
 اَللّٰهُمَّ أَرِحْنِي رَائِحَةَ الجَـنَّةِ وَاَنْتَ عَنِّي رَاضٍ
-
+</div>
 Allohumma Arihni Roihatal Jannati wa anta annii rodliin
-
+<br/>
 Artinya:
 "Ya Allah berikan aku penciuman wewangian syurga dan keadaan Engkau terhadap diriku yang selalu meridhoi"
+<br/>
 
+<h2>
 Niat Berwudhu
-
+</h2>
+<div class="arabic">
 نَوَيْتُ الْوُضُوْءَ لِرَفْعِ الْحَدَثِ اْلاَصْغَرِ فَرْضًا لِلّٰهِ تَعَالَى
-
+</div>
 Nawaitul whudu-a lirof'il hadatsii ashghori fardhon lillaahi ta'aalaa
-
+<br/>
 Artinya :
 "Saya niat berwudhu untuk menghilangkan hadast kecil fardu (wajib) karena Allah ta'ala"
+<br/>
 
+<h2>
 Doa Ketika Membasuh Muka
-(Setelah membaca niat wudhu dalam hati)
-
+<small>(Setelah membaca niat wudhu dalam hati)</small>
+</h2>
+<div class="arabic">
 اَللّٰهُمَّ بَيِّضْ وَجْهِى يَوْمَ تَبْيَضُّ وُجُوْهٌ وَتَسْوَدُّ وُجُوْهٌ
-
+</div>
 Allohumma bayyid wajhiy yauma tabyadu wujuuh wa taswaddu wujuuh
-
+<br/>
 Artinya:
 "Ya Allah putihkan wajahku pada hari menjadi putih berseri wajah-wajah kaum muslimin dan menjadi hitam legam wajah-wajah orang kafir"
 
+<h2>
 Doa Ketika Membasuh Tangan Kanan
-
+</h2>
+<div class="arabic">
 اَللّٰهُمَّ اَعْطِنِى كِتاَبِى بِيَمِيْنِى وَحَاسِبْنِى حِسَاباً يَسِيْرًا
+</div>
 Allohumma A'thini kitabi biyamini wa hasibni hisaban yasiro
-
+<br/>
 Artinya:
 "Ya Allah berikanlah kepadaku kitab amalku dari dari tangan kananku dan hisablah aku dengan penghisaban yang ringan"
 
+<h2>
 Doa Ketika Membasuh Tangan Kiri
-
+</h2>
+<div class="arabic">
 اَللّٰهُمَّ لاَ تُعْطِنِى كِتاَبِى بِشِمَالِى وَلاَمِنْ وَرَاءِ ظَهْرِىْ
-
+</div>
 Allohumma Laa Ta'thini Kitabi bisyimali walaa min waro'i dzohri
-
+<br/>
 Artinya:
 "Ya Allah jangan Engkau berikan kepadaku kitab amal dari tangan kiriku atau pada belakang punggungku"
-
+<br/>
 Baca juga : Doa masuk masjid dan keluar masjid lengkap
 
+<h2>
 Doa Ketika Mengusap Rambut Kepala
-
+</h2>
+<div class="arabic">
 اَللّٰهُمَّ حَرِّمْ شَعْرِيْ وَبَشَرِيْ عَلَى النَّارِ
+</div>
 Allohumma harrim sya'ri wabasyari 'Alannari
-
+<br/>
 Artinya:
 "Ya Allah haramkan rambutku dan kulitku atas api neraka"
+<br/>
 
+<h2>
 Doa Ketika Membasuh Kedua Telinga
-
+</h2>
+<div class="arabic">
 اَللّٰهُمَّ اجْعَلْنِي مِنَ الَّذِيْنَ يَسْتَمِعُوْنَ اْلقَوْلَ فَيَتَّبِعُوْنَ أَحْسَنَهُ
-
+</div>
 Allohummaj'Alni minalladzina yastami'unal Qoula fayattabi'una ahsanahu
-
+<br/>
 Artinya:
 "Ya Allah, jadikanlah aku termasuk orang-orang yang mendengarkan nasehat dan mengikuti sesuatu yang terbaik"
 
+<h2>
 Doa ketika Membasuh Kaki Kanan
-
+</h2>
+<div class="arabic">
  اَللّٰهُمَّ ثَبِّتْ قدَمَيَّ عَلَى الصِّرَاطِ يَوْمَ تُثَبِّتُ فِيْهِ اَقْدَامَ عِبَادِكَ الصَالِحِينَ
-
+</div>
 Allohumma Tsabbit Qodamayya 'Alaas Syirothi yauma tutsabbitu fiihi Aqdama 'ibaadikas shoolihiin
-
+<br/>
 Artinya:
 "Ya Allah, mantapkan kedua kakiku di atas titian (shiroth
