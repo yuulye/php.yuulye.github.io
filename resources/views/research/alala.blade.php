9e08ed7d7e2761ab669c3d01f5e49df4ec567575
yuulye <yuulye@gmail.com>
2019-04-27 09:25:55 +0700

    alala

---
+++ resources/views/research/alala.blade.php
@@ -0,0 +1,29 @@
+@extends('print')
+
+@section('head')
+  @parent
+
+  <style>
+    table {
+      width: 100%;
+      height: 100%;
+    }
+    table tr td {
+      width: 50%;
+      vertical-align: top;
+      padding: 8px;
+    }
+    table tr td.side1 {
+      border-right: 1px solid black;
+    }
+    table tr td.side2 {
+      border-left: 1px solid black;
+    }
+  </style>
+@endsection
+
+@section('content')
+<div class="page landscape">
+  @include('research.alala.1')
+</div>
+@endsection
---
+++ resources/views/research/alala/1.blade.php
@@ -0,0 +1,105 @@
+<!--
+Kitab Alala
+Nadhom, Arti terjemah, Penjelasan Singkat
+
+Pengarang : Muhammad Abu Basyir Al-Dimawi.
+-->
+
+<table>
+  <tr>
+    <td class="side1">
+      <h2>
+      Syarat-syarat mencari ilmu (nadhom 1 dan 2)
+      </h2>
+<pre class="arabic">اَلاَ لاَتَنَــــالُ الْعِـــلْمَ اِلاَّ بِســــــِتَّةٍ ۞ سَأُنْبِيْكَ عَنْ مَجْمُوْعِهَا بِبَيَانٍ</pre>
+      ELINGO DAK HASIL ILMU ANGING NEM PERKORO  #  BAKAL TAK CRITA'KE KUMPULE KANTI PERTELO
+      <br/>
+      (ingatlah, kamu tidak akan mendapatkan ilmu kecuali dengan enam perkara, akan saya jelaskan semuanya dengan terperinci)
+
+<pre class="arabic">ذُكَاءٍ وَحِرْصٍ وَاصْطِبَارٍوَبُلْغَةٍ ۞ وَاِرْشَادُ اُسْتَاذٍ وَطُوْلِ زَمَانٍ          </pre>
+      RUPANE LIMPAT LOBO SOBAR ONO SANGUNE   #   LAN PIWULANGE GURU LAN SING SUWE MANGSANE
+      <br/>
+      (cerdas, semangat, sabar, dan biaya, serta petunjuk guru dan masa yang lama)
+
+      <br/>
+      <br/>
+      <br/>
+
+      <h2>
+      Pedoman Memilih Teman (Nadhom 3 dan 4)
+      </h2>
+      <div class="arabic">
+      عَنِ الْـمَرْءِ لاَ تَسْأَلْ وَسَلْ عَنْ قَرِيْنِهِ ۞ فَإِنَّ القَرِيْنَ بِالْـمُقَـــــارِنِ يَقْتَــــــــــدِيْ
+      </div>
+      JO TAKON SONGKO WONG SIJI TAKONO KANCANE  #   KeRONO SAKTEMENE KANCA MANUT KANG NGANCANI
+      <br/>
+      (Jangan tanya kepribadian seseorang, tapi lihatlah temannya, karena sesungguhnya teman mengikuti kelakuan temannya)
+      <div class="arabic">
+      فَاِنْ كَانَ ذَا شَرٍّ فَجَنِّبْـــــهُ سُــرْعَةً ۞ فَاِنْ كَانَ ذَاخَيْرٍ فَقَارِنْهُ تَهْتَــــــــــــدِيْ
+      </div>
+      YEN ONO KONCO OLO LAKUNE NDANG DOHONO   #   YEN ONO KONCO BAGUS ENGGAL NDANG KANCANONO
+      <br/>
+      (jika ada teman berkelakuan buruk, segeralah tinggalkan. jika ada teman bagus akhlaknya, segeralah jadikan teman)
+
+      <br/>
+      <br/>
+      <br/>
+
+      <h2>
+      Keutamaan Ilmu (Nadhom 5)
+      </h2>
+      <div class="arabic">
+      تَعَــــــلَّمْ فَاِنَّ اْلعِلْمَ زَيْنٌ لِأَهْلِهِ ۞ وَفَضْلٌ وَعِنْوَانٌ لِكُلِّ الْمَحَامِدِ
+      </div>
+      NGAJIHO KERONO ILMU MAHESI ING AHLINE   #   LAN NGUNGGULKE LAN DADI TONDO TINGKAH PINUJI
+      <br/>
+      (Mengajilah, karena ilmu akan menjadi hiasan bagi pemiliknya, dan keutamaan baginya serta tanda setiap hal yang terpuji)
+    </td>
+    <td class="side2">
+      <h2>
+      Metode mencari ilmu (Nadhom 6)
+      </h2>
+      <pre class="arabic">وَكُنْ مُسْتَفِيْدًا كُلَّ يَوْمٍ زِيـَـــــادَةً ۞ مِنَ الْعِلْمِ وَاسْبحْ فِىْ بُحُوْرِ الْفَوَائِدِ</pre>
+      ONOHO NGALAP FAEDAH SABEN DINO ING TAMBAH  #   SONGKO ILMU LAN NGELANGI SEGARANE FAEDAH
+      <br/>
+      (jadilah kamu orang yang mencari faidah, setiap harinya bertambah ilmu dan berenang di atas lautan faidah)
+
+      <br/>
+      <br/>
+      <br/>
+
+      <h2>
+      Ilmu Fiqih dan keutamaannya (Nadhom 7 dan 8)
+      </h2>
+      <pre class="arabic">تَفَقَّـهْ فَاِنَّ اْلفِقْــــهَ اَفْضَلٌ قَائِـدِ ۞ اِلَى الْبِّرِوَالتَّقْوَى وَاَعْدَلُ قَاصِدِ</pre>
+      NGAJIO FIQIH KeRONO NGUNGGULKE LAN NUDUHKE   #   MARING BAGUS LAN WEDI ALLAH LUWIH JEJEKE
+      <br/>
+      (Belajarlah ilmu Fiqih karena Fiqih adalah paling utamanya penuntun menuju kebaikan dan ketakwaan, dan paling lurusnya sesuatu yang lurus)
+      <pre class="arabic">هُوَاْلعِلْمُ اْلهَادِىْ اِلَى سُنَنِ الْهُـــــدَى ۞ هُوَالْحِصْنُ يُنْجِىْ مِنْ جَمِيْعِ الشَّدَائِدِ</pre>
+      ILMU FIQIH KANG NUDUHKE DALANE PITUDUH   #   HIYO BENTENG KANG NYELAMETKE SEKEHE PEKEWUH
+      <br/>
+      (ilmu Fiqih yang menunjukkan ke jalan petunjuk, yaitu benteng yang menyelamatkan dari semua yang menyusahkan/memberatkan)
+
+      <br/>
+      <br/>
+      <br/>
+
+      <h2>
+      Keutamaan ahli Fiqih dari ahli Ibadah (Nadhom 9)
+      </h2>
+      <pre class="arabic">فَـــاِنَّ فَقِيْهًــا وَاحِـــدًامُتَوَرِّعًـــــــــا ۞ اَشَدُّعَلَى الشَّيْطَانِ مِنْ اَلْفِ عَابِدِ</pre>
+      WONG ALIM FIQIH SIJI TUR KANG NGEDOHI HAROM  #    LUWIH ABOT TIMBANG 'ABID SEWU MUNGGUH SETAN
+      <br/>
+      (sesungguhnya satu orang ahli Fiqih yang wara' (menghindari barang haram) itu lebih berat bagi Setan dibandingkan 1000 ahli ibadah)
+
+    </td>
+  </tr>
+</table>
+
+
+
+<!--
+Bahayanya orang bodoh yang tekun ibadah (nadhom 10 dan 11)
+
+فَسَــادٌ كَبِيْرٌ عَــــال�
+--!>
