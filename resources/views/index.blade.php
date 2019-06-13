40b65e7faf43fd0a2624bc2d719cbfb2cd16a8ed
yuulye <yuulye@gmail.com>
2019-06-13 11:16:31 +0700

    base redesign

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,89 +1,38 @@
 @extends ('base')
 
 @section ('head')
-  <style>
-    body {
-      font-family: sans-serif;
-    }
-
-    h1, h2, h3, h4, h5, h6 {
-      font-family: serif;
-    }
-
-    #nav {
-      font-size: 32px;
-      text-align: center;
-    }
-    #nav a {
-      display: inline-block;
-      text-decoration: none;
-      padding: 8px;
-    }
-
-    @media (min-width: 576px)   {}
-    @media (min-width: 768px)   {}
-    @media (min-width: 992px)   {}
-    @media (min-width: 1200px)  {}
-  </style>
+  <style>@include ('index.style')</style>
 @endsection
 
 @section ('content')
-  <dl>
+  <div class="posts">
     @include ('index.post.tictactoe_ai')
     @foreach ($posts as $post)
       @include ('index.post')
     @endforeach
-  </dl>
-  @if (!@$_GET['debug'])
-    <div id="comment" style="padding: 16px;">
-      @include('disqus')
-    </div>
-  @endif
-  @include ('index.hidden')
+  </div>
 
-  @include ('index.hr')
-  <br/>
-  <br/>
-  <br/>
-  <div id="nav">
-    <a href="#comment">
-      <i class="fa fa-comment"></i>
-    </a>
-    <a href="#comment">
-      <i class="fa fa-twitter"></i>
-    </a>
-    <a href="#comment">
-      <i class="fa fa-facebook"></i>
-    </a>
-    <a href="#comment">
-      <i class="fa fa-wordpress"></i>
-    </a>
-    <a href="#comment">
-      <i class="fa fa-youtube"></i>
-    </a>
-    <a href="#comment">
-      <i class="fa fa-instagram"></i>
-    </a>
-    <a href="#comment">
-      <i class="fa fa-amazon"></i>
-    </a>
-    <a href="#comment">
-      <i class="fa fa-android"></i>
-    </a>
+  <div class="comments">
+    <br/>
+    @include('disqus')
+    <br/>
+    <br/>
   </div>
-  <div style="height: 256px;">
-    <table style="width: 100%; height: 100%">
-      <tr>
-        <td align="center">
-          <i>~</i>
-        </td>
-      </tr>
-    </table>
+
+  <hr/>
+
+  <div align="center">
+    <br/>
+    <br/>
+      &copy;
+      {{
+        config('app.name')
+        . ". 2017 - " . date('Y')
+      }}.
+    <br/>
+    <br/>
+    <br/>
   </div>
-  <div align="center" style="padding: 8px;">
-    <code>
-      &copy; {{ config('app.name') . ". " . date('Y') }}.
-    </code>
-  </div>
-  <br/>
+
+  @include ('index.hidden')
 @endsection
--- resources/views/index/post.blade.php
+++ resources/views/index/post.blade.php
@@ -1,26 +1,26 @@
-<dt style="
-  padding: 8px 16px;
-  white-space: nowrap;
-  overflow: hidden;
-  text-overflow: ellipsis;
-">
-  <code>{{
-    !is_array($post)?
-    $post->date->format('Y M d'):$post[0]
-  }}</code>
-  &middot;
-  <h4 style="display: inline;">{{
-    !is_array($post)?
-    $post->data->title:$post[1]
-  }}</h4>
-</dt>
-<dd style="
-  padding: 8px 16px;
-  margin: 0;
-">
-  {{ !is_array($post)?$post->data->excerpt:$post[2] }}
-  <a href="{{ url(
-    (!is_array($post)?'/post/'.$post->path:$post[3])
-  ) }}">more...</a>
-</dd>
-@include ('index.hr')
+<div class="item">
+  <br/>
+  <div class="header">
+    <code>{{
+      !is_array($post)?
+      $post->date->format('Y m/d'):$post[0]
+    }}</code>
+    &middot;
+    <b>{{
+      !is_array($post)?
+      $post->data->title:$post[1]
+    }}</b>
+  </div>
+  <br/>
+
+  <div class="content">
+    {{ !is_array($post)?$post->data->excerpt:$post[2] }}
+    <a href="{{ url(
+      (!is_array($post)?'/post/'.$post->path:$post[3])
+    ) }}">more...</a>
+  </div>
+  <br/>
+</div>
+
+<hr/>
+
