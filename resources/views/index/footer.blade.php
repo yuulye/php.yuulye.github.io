6c580109100e1b04e6dd6cd30c1752b7a22c6a20
yuulye <yuulye@gmail.com>
2019-06-14 19:28:53 +0700

    resolve #3 social media links; close #2 no code

--- resources/views/index/footer.blade.php
+++ resources/views/index/footer.blade.php
@@ -1,4 +1,44 @@
-<div align="center">
+@php
+  $socials = [
+    [
+      "GitHub",
+      "https://github.com/yuulye",
+    ],
+    [
+      "SoloLearn",
+      "https://www.sololearn.com/Profile/14417238",
+    ],
+    [
+      "DEV Community",
+      "https://dev.to/yuulye",
+    ],
+    [
+      "Twitter",
+      "https://twitter.com/lyeyuu",
+    ],
+    [
+      "Facebook",
+      "https://www.facebook.com/yuulye",
+    ],
+  ];
+@endphp
+<div class="social" align="center">
+  <br/>
+  @foreach ($socials as $s)
+    <a href="{{ $s[1] }}">{{ $s[0] }}</a>
+    @if (!$loop->last)
+      <br/>
+      &middot;
+      <br/>
+    @endif
+  @endforeach
+  <br/>
+  <br/>
+</div>
+
+<hr/>
+
+<div align="center" class="footer">
   <br/>
   <br/>
     &copy;{{
