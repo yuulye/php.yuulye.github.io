e1f626868a3ef7ae5b895f17575ab036dffda848
yuulye <yuulye@gmail.com>
2019-06-24 01:21:20 +0700

    code: toFixed.js

---
+++ storage/codes/toFixed.js
@@ -0,0 +1,15 @@
+/*
+ * SoloLearn.com - Challenge
+ * JavaScript
+ * by Kartikey Sahu
+ */
+
+let sum = 0.1 + 0.2;
+console.log( +sum.toFixed(2) );
+
+//-----------------------------
+
+a = sum.toFixed(2);
+b = +sum.toFixed(2);
+console.log(a, typeof a, b, typeof b);
+
