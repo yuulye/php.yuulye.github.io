effd501da350bb9cd9eebad33fc70cb4f7696c84
yuulye <yuulye@gmail.com>
2019-06-24 01:35:11 +0700

    code: x y NaN

---
+++ storage/codes/x_y_NaN.js
@@ -0,0 +1,25 @@
+/*
+ * SoloLearn.com
+ * JavaScript Challenge
+ * by brains
+ */
+
+var x = NaN;
+var y = x;
+if (x === y)
+    console.log(true);
+else console.log(false);
+
+//-----------------------------
+
+console.log(x, typeof x, y, typeof y, x === y);
+
+a = 0;
+b = a;
+console.log(a, typeof a, b, typeof b, a === b);
+
+a = Infinity;
+b = a;
+console.log(a, typeof a, b, typeof b, a === b);
+
+
